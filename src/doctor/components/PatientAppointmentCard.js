10c599651815d7cb838a255eade2df6445af1c66
Amit Varshney <amit.varshney1@yahoo.co.in>
2021-08-20 11:00:02 +0530

    added clickable card for PatientAppointmentCard.js

--- src/doctor/components/PatientAppointmentCard.js
+++ src/doctor/components/PatientAppointmentCard.js
@@ -1,13 +1,14 @@
-import { Card, CardContent, CardMedia } from "@material-ui/core";
+import { Card } from "@material-ui/core";
 import {calendar,clock} from "../../constants/DoctorImages";
 import {Link} from 'react-router-dom';
 import {Image} from "react-bootstrap";
-import {icon_man} from "../../constants/PatientImages";
 
 const PatientAppointmentCard = (props) => {
   return (
     <>
-    <Card className="patient-appointment-container">
+    <Card className="patient-appointment-container" onClick={() =>{
+        console.log('user clicked' );
+    }}>
         <div className="image-container">
             <img src={props?.image} style={{
                 maxWidth:"100%",
@@ -40,42 +41,7 @@ const PatientAppointmentCard = (props) => {
                 <span className="time-info space-10">{props.onDate}</span>
             </div>
             </div>
-
-
         </div>
-
-
-        {/*<div style={{display: 'flex', flexDirection: 'row'}}>*/}
-        {/*  <div style={{backgroundColor: 'pink', margin:20}}>*/}
-        {/*  <CardMedia className="doctor-card-image" image={props?.image}></CardMedia>*/}
-        {/*  </div>*/}
-        {/*  <div>*/}
-        {/*    <CardContent>*/}
-        {/*        <span className="doctor-card-doctor-name">{props.name}</span>*/}
-        {/*        <span className="doctor-card-doctor-details">{props?.details}</span>*/}
-        {/*        <span>*/}
-        {/*          <span className="doctor-card-fee-label">Fee:</span>*/}
-        {/*          <span className="doctor-card-fee-value">Rs {props.fees}</span>{" "}*/}
-        {/*        </span>*/}
-        {/*        /!*<span style={{marginTop: "5px" }}>*!/*/}
-        {/*        /!*<span*!/*/}
-        {/*        /!*    className="doctor-card-specialization-text"*!/*/}
-        {/*        /!*    style={{  marginTop: "10px" }}*!/*/}
-        {/*        /!*  >*!/*/}
-        {/*        /!*    {*!/*/}
-        {/*        /!*      props?.qualifications.map((s) => {*!/*/}
-        {/*        /!*          return(<span key={s} style={{ padding: '5px', marginRight: '10px'}} className='doctor-card-specialization-container'>{s}</span>)*!/*/}
-        {/*        /!*      })*!/*/}
-        {/*        /!*    }*!/*/}
-
-        {/*        /!*  </span>*!/*/}
-        {/*        /!*</span>*!/*/}
-        {/*        /!*<Link to={`/patient/doctorDetails/${props.id}`}>*!/*/}
-        {/*        /!*  <span className="doctor-card-details-link">View Details</span>*!/*/}
-        {/*        /!*</Link>*!/*/}
-        {/*    </CardContent>*/}
-        {/*  </div>*/}
-        {/*</div>*/}
       </Card>
     </>
   );
