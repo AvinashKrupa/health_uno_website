726e7a54bb26dfc6ffd40233af0c79e3e9c1eed1
Amit Varshney <amit.varshney1@yahoo.co.in>
2021-08-31 11:09:53 +0530

    Added doctor profile base page container

---
+++ src/doctor/view/Profile/DoctorProfile.js
@@ -0,0 +1,40 @@
+import {Col, Row} from "react-bootstrap";
+import React from "react";
+import ProfilePictureColumn from "./EditProfilePictureColumn";
+import EditProfilePage from "./DoctorEditProfile";
+import Invite from "../../../patient/view/invite/Invite";
+import Chat from "../../../chat/Chat";
+
+const DoctorProfile = (props) => {
+    const type = props.match.params.type
+
+    return (
+        <div className='form-wizard edit-profile-container'>
+            <Row>
+                <Col lg="4">
+                    <ProfilePictureColumn/>
+                </Col>
+                <Col lg='6'>
+                    {
+                        type === 'editProfile' && (
+                            <EditProfilePage></EditProfilePage>
+                        )
+                    }
+                    {
+                        type === 'invite' && (
+                            <Invite></Invite>
+                        )
+                    }
+                    {
+                        type === 'support' && (
+                            <Chat></Chat>
+                        )
+                    }
+                </Col>
+                <Col lg='2'> </Col>
+            </Row>
+        </div>
+
+    );
+};
+export default DoctorProfile;
