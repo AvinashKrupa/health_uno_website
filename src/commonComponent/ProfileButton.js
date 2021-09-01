d872cf93cb55c8416f6831c50fc495327daece31
Prakash <prakash@cnetric.com>
2021-09-01 14:41:08 +0530

    fix responsive issue in profile page

--- src/commonComponent/ProfileButton.js
+++ src/commonComponent/ProfileButton.js
@@ -7,7 +7,7 @@ const ProfileButton = ({fontText,btnText, onClick, className,active, route}) => 
             display: "flex",
             background: active === route ? `rgba(${40}, ${163}, ${218}, ${0.2})` : ''
         }} className={'patient-profile-btn ' + className} onClick={onClick}>
-             <i class={`fas ${fontText}`} style={{marginLeft: '30px', marginTop: '4px'}}></i>
+             <i class={`fas ${fontText}`} style={{ marginTop: '4px'}}></i>
              <span style={{marginLeft: '15px', }}>{btnText}</span>
          </div>
       </>
