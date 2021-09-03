8fc494fe01751277b25a3356b3a315fc2181f465
Prakash <prakash@cnetric.com>
2021-09-03 11:41:17 +0530

    load data on scrolling in top consultant page

--- src/patient/view/Home/TopConsultants.js
+++ src/patient/view/Home/TopConsultants.js
@@ -13,6 +13,7 @@ import { Button } from "react-bootstrap";
 import { filter } from "../../../constants/PatientImages";
 import SimilarDoctorsCard  from '../doctorDetail/SimilarDoctorsCard'
 import { back_icon } from "../../../constants/DoctorImages";
+import InfiniteScroll from"react-infinite-scroll-component";
 
 const TopConsultants = (props) => {
   let timer = null;
@@ -20,12 +21,12 @@ const TopConsultants = (props) => {
   let [searchText, setSearchText] = useState(useSearchStore(state => state.searchText));
   let [consultants, setConsultant] = useState([]);
   const [sidebarOpen, setSidebarOpen] = useState(false);
-
+  const [page, setPage] = useState(1);
   useEffect(() => {
     getTopConsultants();
   }, [searchText]);
 
-  
+
   function callBackFilter(data) {
     getTopConsultants(data.sortBy, data.min, data.max, data.selectedLanguages);
   }
@@ -33,7 +34,7 @@ const TopConsultants = (props) => {
   function getTopConsultants(sortBy = 'asc', min = '', max = '' , lang = '') {
     let params = {
       limit: 10,
-      page: 1,
+      page: page,
       filter: {
         text: searchText,
         fee_min: min,
@@ -47,7 +48,9 @@ const TopConsultants = (props) => {
     post(API.GETTOPCONSULTANT, params)
       .then(response => {
         if (response.status === 200) {
-          setConsultant(response.data.data.docs);
+          console.log([...consultants, ...response.data.data.docs], "response.data.data.docs");
+          setConsultant([...consultants, ...response.data.data.docs]);
+          setPage(page + 1)
         } else {
           addToast(response.data.message, { appearance: "error" });
         }
@@ -58,19 +61,21 @@ const TopConsultants = (props) => {
   }
 
   function debounce(txt) {
-      clearTimeout(timer);
-      timer = setTimeout(function() {
-        setSearchText(txt);
-      }, 1000);
+    clearTimeout(timer);
+    timer = setTimeout(function() {
+      setSearchText(txt);
+    }, 1000);
   }
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   }
-
+  const fetchMoreData = () => {
+      getTopConsultants()
+  };
   return (
     <div>
-      <TopConsultantsFilter sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} callBackFilter={callBackFilter}/>
+      <TopConsultantsFilter sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} callBackFilter={callBackFilter} />
       <Row className='top-consultants-container'>
         <Col lg="1"  sm="1" xs='1' />
         <Col lg="10" sm="10" xs='10' className='screen-768'>
@@ -94,28 +99,38 @@ const TopConsultants = (props) => {
               <img src={filter} alt='filter-img' style={{height: '26px', width: '24px'}}></img>
             </Button>
           </div>
-         
-          <Row style={{display: 'flex', flexDirection: 'row'}} className='top-consultants-card-container'>
-          {consultants.map((doctor) => {
-              return(
-                <Grid container item lg={4}  md={6} sm={6} xs={12} spacing={1}>
-                   <SimilarDoctorsCard
+          <div>
+            <InfiniteScroll
+              dataLength={consultants.length}
+              next={fetchMoreData}
+              hasMore={true}
+              className="load-data"
+              
+            >
+              <Row style={{ display: 'flex', flexDirection: 'row' }} className='top-consultants-card-container'>
+                {consultants.map((doctor) => {
+                  return(
+                    <Grid container item lg={4}  md={6} sm={6} xs={12} spacing={1}>
+                      <SimilarDoctorsCard
                         id={doctor._id}
                         image={doctor.dp}
                         name={`${doctor.first_name} ${doctor.last_name}`}
                         fees={doctor.fee}
                         details={`${doctor.city}, ${doctor.country} | ${doctor.exp} Y Exp`}
                         qualifications={doctor.specialities}
-                  />
-                </Grid>
-              )
-            })}
-          </Row>
+                      />
+                    </Grid>
+                  )
+                })}
+              </Row>
+            </InfiniteScroll>
+          </div>
+
         </Col>
-        <Col lg="1" sm="1" xs='1'/>
+        <Col lg="1" sm="1" xs='1' />
       </Row>
-    </div>    
+    </div>
   );
 };
 
-export default TopConsultants;  
+export default TopConsultants;
