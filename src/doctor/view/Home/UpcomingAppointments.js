93e147c15eb062c8ab132a417e93d2aee1f6b7f4
Amit Varshney <amit.varshney1@yahoo.co.in>
2021-09-12 17:07:24 +0530

    Fixed date format, Added upcoming and previous tabs in search and view all appointments in doctor side

--- src/doctor/view/Home/UpcomingAppointments.js
+++ src/doctor/view/Home/UpcomingAppointments.js
@@ -16,28 +16,33 @@ const UpcomingAppointments = (props) => {
   let timer = null;
   const {addToast} = useToasts();
   let [searchText, setSearchText] = useState(useSearchStore(state => state.searchText));
-  let [appointments, setAppointments] = useState([]);
-  const [currentDate, setCurrentDate] = useState(moment().format('YYYY-MM-DD'));
+  const [currentDate, setCurrentDate] = useState(props.location?.state?.title === 'Search Appointments'? moment().format('YYYY-MM-DD') : "");
   let [appointmentLoaderStatus, setAppointmentLoaderStatus] = useState(false);
+  const [upcoming, setUpcoming] = useState(true);
+  const [previous, setPrevious] = useState(false);
+  const [upcomingAppointments, setUpcomingAppointments] = useState([]);
+  const [previousAppointments, setPreviousAppointments] = useState([]);
 
   useEffect(() => {
     getUpcomingAppointments();
+    getPreviousAppointments();
   }, [searchText]);
 
   useEffect(() => {
     getUpcomingAppointments();
+    getPreviousAppointments();
   }, [currentDate]);
 
-
-  function callBackFilter(data) {
-    getUpcomingAppointments(data.sortBy, data.min, data.max, data.selectedLanguages);
-  }
+  const handleSelection = () => {
+    setUpcoming(!upcoming);
+    setPrevious(!previous);
+  };
 
   function getUpcomingAppointments() {
     let params = {
       limit: 20,
       page: 1,
-      sort_order: 'asc',
+      sort_order: 'desc',
       sort_key: "created_at",
       search_text: searchText,
       date: currentDate,
@@ -55,7 +60,7 @@ const UpcomingAppointments = (props) => {
         .then(response => {
           if (response.status === 200) {
             setAppointmentLoaderStatus(false)
-            setAppointments(response.data.data.docs);
+            setUpcomingAppointments(response.data.data.docs);
           } else {
             addToast(response.data.message, {appearance: "error"});
             setAppointmentLoaderStatus(false)
@@ -64,6 +69,37 @@ const UpcomingAppointments = (props) => {
         .catch(error => {
           addToast(error.response.data.message, {appearance: "error"});
           setAppointmentLoaderStatus(false)
+        });
+  }
+
+  function getPreviousAppointments() {
+    let params = {
+      limit: 20,
+      page:1,
+      sort_order: "desc",
+      sort_key: "created_at",
+      search_text: searchText,
+      date: currentDate,
+      status: [
+        "pending",
+        "scheduled",
+        "cancelled",
+        "rejected",
+        "ongoing",
+        "completed"
+      ]
+    }
+
+    post(API.DOCTOR_GET_APPOINTMENTS_API, params)
+        .then(response => {
+          if (response.status === 200 && response.data && response.data.data) {
+            setPreviousAppointments(response.data.data.docs)
+          } else {
+            addToast(response.data.message, { appearance: 'error' });
+          }
+        })
+        .catch(error => {
+          addToast(error.response.data.message, { appearance: 'error' });
         });
   }
 
@@ -77,7 +113,7 @@ const UpcomingAppointments = (props) => {
   const CustomDateComponent = forwardRef(({ value, onClick }, ref) => (
       <>
         <div className="calender-date-upcoming" onClick={onClick} ref={ref} style={{display: 'flex', flexDirection:"row"}}>
-          {moment(value).format("MMM YYYY")}<div style={{marginLeft:10}}><Image src={calendar_blue} /></div>
+          {moment(value).format("ddd, DD MMM YY")}<div style={{marginLeft:10}}><Image src={calendar_blue} /></div>
         </div>
       </>
   ));
@@ -93,16 +129,15 @@ const UpcomingAppointments = (props) => {
                     <img src={back_icon} alt='back_icon-img' onClick={()=>props.history.goBack()}></img>
                     <span>{props.location?.state?.title || 'All Appointments'}</span>
                   </div>
-                  <div className="calendar-container">
+                  {props.location?.state?.title === 'Search Appointments' && <div className="calendar-container">
                     <DatePicker
                         selected={new Date(currentDate)}
                         onChange={(date) => setCurrentDate(date)}
                         maxDate={new Date(new Date().getFullYear(), new Date().getMonth() + 1, 15)}
-                        minDate={new Date(new Date().getFullYear(), new Date().getMonth(), 15)}
                         placeholderText="Select a date before 30 days in the future"
-                        customInput={<CustomDateComponent />}
+                        customInput={<CustomDateComponent/>}
                     />
-                  </div>
+                  </div>}
                 </div>
               </Row>
               <div className='search-container' style={{display: "flex", justifyContent: 'space-between'}}>
@@ -116,34 +151,90 @@ const UpcomingAppointments = (props) => {
                 </SearchInputWithIcon>
               </div>
 
-              <Row style={{display: 'flex', flexDirection: 'row'}} className='top-consultants-card-container'>
-                {appointmentLoaderStatus &&
-                <div className="empty-list-container">
-                  <Spinner showLoader={appointmentLoaderStatus} width={60} height={60}/>
-                </div>
+              <Row>
+            <span
+                className={
+                  upcoming
+                      ? "appointment-page-text-selected"
+                      : "appointment-page-text-unselected"
                 }
-                {!appointmentLoaderStatus && appointments.map((doctor) => {
-                  return (
-                      <Grid container item lg={4} md={6} sm={6} xs={12} spacing={1}>
-                        <PatientAppointmentCard
-                            key={doctor._id}
-                            id={doctor._id}
-                            image={doctor.dp}
-                            // name={`${doctor.first_name} ${doctor.last_name}`}
-                            name={`${doctor.first_name}`}
-                            purpose={doctor.reason}
-                            status={doctor.status}
-                            onTime={doctor.time.slot}
-                            onDate={doctor.time.date}
-                        />
-                      </Grid>
-                  )
-                })}
-                {!appointmentLoaderStatus && !appointments.length &&
-                  <div className="empty-list-container">
-                    <h4>No appointments found</h4>
-                  </div>
-                }
+                onClick={handleSelection}
+            >
+              Upcoming
+            </span>
+                <span
+                    className={
+                      previous
+                          ? "appointment-page-text-selected appointment-page-text-margin-left-41"
+                          : "appointment-page-text-unselected appointment-page-text-margin-left-41"
+                    }
+                    onClick={handleSelection}
+                >
+              Previous
+            </span>
+              </Row>
+              <Row className="appointment-page-cards-row">
+                {upcoming ? (
+                    <>
+                      {appointmentLoaderStatus &&
+                      <div className="empty-list-container">
+                        <Spinner showLoader={appointmentLoaderStatus} width={60} height={60}/>
+                      </div>
+                      }
+                      {!appointmentLoaderStatus && upcomingAppointments.map((doctor) => {
+                        return (
+                            <Grid container item lg={4} md={6} sm={6} xs={12} spacing={1}>
+                              <PatientAppointmentCard
+                                  key={doctor._id}
+                                  id={doctor._id}
+                                  image={doctor.dp}
+                                  // name={`${doctor.first_name} ${doctor.last_name}`}
+                                  name={`${doctor.first_name}`}
+                                  purpose={doctor.reason}
+                                  status={doctor.status}
+                                  onTime={doctor.time.slot}
+                                  onDate={doctor.time.date}
+                              />
+                            </Grid>
+                        )
+                      })}
+                      {!appointmentLoaderStatus && !upcomingAppointments.length &&
+                      <div className="empty-list-container">
+                        <h4>No appointments found</h4>
+                      </div>
+                      }
+                    </>
+                ) : (
+                    <>
+                      {appointmentLoaderStatus &&
+                      <div className="empty-list-container">
+                        <Spinner showLoader={appointmentLoaderStatus} width={60} height={60}/>
+                      </div>
+                      }
+                      {!appointmentLoaderStatus && previousAppointments.map((doctor) => {
+                        return (
+                            <Grid container item lg={4} md={6} sm={6} xs={12} spacing={1}>
+                              <PatientAppointmentCard
+                                  key={doctor._id}
+                                  id={doctor._id}
+                                  image={doctor.dp}
+                                  // name={`${doctor.first_name} ${doctor.last_name}`}
+                                  name={`${doctor.first_name}`}
+                                  purpose={doctor.reason}
+                                  status={doctor.status}
+                                  onTime={doctor.time.slot}
+                                  onDate={doctor.time.date}
+                              />
+                            </Grid>
+                        )
+                      })}
+                      {!appointmentLoaderStatus && !previousAppointments.length &&
+                      <div className="empty-list-container">
+                        <h4>No appointments found</h4>
+                      </div>
+                      }
+                    </>
+                )}
               </Row>
             </Col>
             <Col lg="1" sm="1" xs='1'/>
