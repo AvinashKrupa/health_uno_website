26dbf7513f3e0e755c5dd91309f4226a65fd182c
Amit Varshney <amit.varshney1@yahoo.co.in>
2021-09-21 16:31:46 +0530

    Added logic to hide timer when time completes

--- src/commonComponent/MeetingTimer.js
+++ src/commonComponent/MeetingTimer.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Countdown from 'react-countdown';
 
 const MeetingTimer = ({date, handleEnableButton}) => {
-    debugger
+    const [showTimerCountdown, setShowTimerCountdown] = useState(true);
     const renderer = ({ hours, minutes, seconds, completed }) => {
       if (completed) {
+          setShowTimerCountdown(false)
         handleEnableButton()
         return null;
       } else {
-        return <div className="meeting-timer-container">{`Meeting starts in ${hours}:${minutes}:${seconds}`}</div>;
+        return (showTimerCountdown && <div className="meeting-timer-container">{`Meeting starts in ${hours}:${minutes}:${seconds}`}</div>);
       }
     };
 
