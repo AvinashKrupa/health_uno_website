9ba8c7bc9bd2d5ca36adde0a528953d15b445ed7
Amit Varshney <amit.varshney1@yahoo.co.in>
2021-09-23 14:19:50 +0530

    Added navigation logic from pre page

--- src/patient/view/PrePage.js
+++ src/patient/view/PrePage.js
@@ -1,17 +1,5 @@
-
-
-import React, { useContext, useState } from "react";
-import {
-    Container,
-    Row,
-    Col,
-    Dropdown,
-    Button,
-    DropdownButton,
-    FormControl,
-    InputGroup,
-    Image,
-} from "react-bootstrap";
+import React from "react";
+import {Button, Col, Container, Row,} from "react-bootstrap";
 
 
 const PrePage = ({history}) => {
@@ -19,38 +7,40 @@ const PrePage = ({history}) => {
 
     return (
         <div className="landingPrePage">
-        <Container>
-        <Row className="PrePageDesign">
-            <Col>
-            <div className="boxLeft">
-                <div>
-                  <img alt='logo' src={process.env.PUBLIC_URL + '/assets/doctor.png'}></img>
-                </div>
-                
-                <div className="boxTextStyle">
-                    <h4>Are you a Doctor?</h4>
-                    <p>Please select the following button to </p> <p>register as a doctor</p>
-                    <Button variant="primary">I am a Doctor</Button>
-                </div>
-            </div>
-            
-            </Col>
-            <Col>
-            <div className="boxRight">
-                <div>
-                <img alt='logo' src={process.env.PUBLIC_URL + '/assets/patient.png'}></img>
-                </div>
-                <div className="boxTextStyle">
-                    <h4>Are you a Patient?</h4>
-                    <p>Please select the following button to </p> <p> register as a patient</p>
-                    <Button variant="primary">I am a Patient</Button>
-                </div>
+            <Container>
+                <Row className="PrePageDesign">
+                    <Col>
+                        <div className="boxLeft">
+                            <div>
+                                <img alt='logo' src={process.env.PUBLIC_URL + '/assets/doctor.png'}></img>
+                            </div>
 
-            </div>
-            </Col>
-        </Row>
-        
-        </Container>
+                            <div className="boxTextStyle">
+                                <h4>Are you a Doctor?</h4>
+                                <p>Please select the following button to </p> <p>register as a doctor</p>
+                                <Button variant="primary" onClick={() => history.push(`/doctor/`)}>I am a
+                                    Doctor</Button>
+                            </div>
+                        </div>
+
+                    </Col>
+                    <Col>
+                        <div className="boxRight">
+                            <div>
+                                <img alt='logo' src={process.env.PUBLIC_URL + '/assets/patient.png'}></img>
+                            </div>
+                            <div className="boxTextStyle">
+                                <h4>Are you a Patient?</h4>
+                                <p>Please select the following button to </p> <p> register as a patient</p>
+                                <Button variant="primary" onClick={() => history.push(`/patient/`)}>I am a
+                                    Patient</Button>
+                            </div>
+
+                        </div>
+                    </Col>
+                </Row>
+
+            </Container>
         </div>
     );
 };
