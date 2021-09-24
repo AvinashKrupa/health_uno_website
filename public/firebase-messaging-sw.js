19f1ea82a67fb8869bed24dbd2abebbeaf45d8bf
Amit Varshney <amit.varshney1@yahoo.co.in>
2021-09-24 12:03:29 +0530

    Added service worker for firebase

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC13l6rYFIL_bhA4hRrnspnN10GyJ3nsTQ",
+    authDomain: "healthuno-df503.firebaseapp.com",
+    projectId: "healthuno-df503",
+    storageBucket: "healthuno-df503.appspot.com",
+    messagingSenderId: "591978792926",
+    appId: "1:591978792926:web:1370ed45774e70f2107f4f",
+    measurementId: "G-XQB6WT9DBL"
+};
+firebase.initializeApp(firebaseConfig);
