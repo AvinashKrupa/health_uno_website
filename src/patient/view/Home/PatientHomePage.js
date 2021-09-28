a4a7db763a2c5601f63f076b0aaf67c54528635a
Amit Varshney <amit.varshney1@yahoo.co.in>
2021-09-28 15:05:57 +0530

    Fixed HUNO-269 : When there is no Top Consultants or speciality, show not found message. Also add logic to show view all button

--- src/patient/view/Home/PatientHomePage.js
+++ src/patient/view/Home/PatientHomePage.js
@@ -1,12 +1,9 @@
-import DoctorCard from "../../commonComponentPatient/DoctorCard";
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
-import { Row, Col } from "react-bootstrap";
+import {Col, Row} from "react-bootstrap";
 import SpecialityCard from "../../commonComponentPatient/SpecialityCard";
-import { API, get, post } from "../../../api/config/APIController";
-import { useEffect } from "react";
-import { useToasts } from "react-toast-notifications";
-import { useState } from "react";
+import {API, get, post} from "../../../api/config/APIController";
+import {useEffect, useState} from "react";
+import {useToasts} from "react-toast-notifications";
 import CarouselComponent from "../../../commonComponent/CarouselComponent";
 import Grid from "@material-ui/core/Grid";
 import useSearchStore from "../../store/searchStore";
@@ -14,10 +11,9 @@ import SearchInputWithIcon from "../../../commonComponent/SearchInputWithIcon";
 import SimilarDoctorsCard from "./../doctorDetail/SimilarDoctorsCard";
 
 
-
 const PatientHomePage = (props) => {
   let timer = null;
-  const { addToast } = useToasts();
+  const {addToast} = useToasts();
   const setSearchText = useSearchStore((state) => state.setSearchText);
   let [specialities, setSpecialities] = useState([]);
   let [slider, setSlider] = useState([]);
@@ -31,31 +27,31 @@ const PatientHomePage = (props) => {
 
   function getHomeContent() {
     get(API.GETHOMECONTENT)
-      .then((response) => {
-        if (response.status === 200) {
-          setSlider(response.data.data.slider);
-          setSpecialities(response.data.data.specialities);
-        } else {
-          addToast(response.data.message, { appearance: "error" });
-        }
-      })
-      .catch((error) => {
-        addToast(error.response.data.message, { appearance: "error" });
-      });
+        .then((response) => {
+          if (response.status === 200) {
+            setSlider(response.data.data.slider);
+            setSpecialities(response.data.data.specialities);
+          } else {
+            addToast(response.data.message, {appearance: "error"});
+          }
+        })
+        .catch((error) => {
+          addToast(error.response.data.message, {appearance: "error"});
+        });
   }
 
   function getTopConsultants() {
     post(API.GETTOPCONSULTANT)
-      .then((response) => {
-        if (response.status === 200) {
-          setConsultant(response.data.data.docs);
-        } else {
-          addToast(response.data.message, { appearance: "error" });
-        }
-      })
-      .catch((error) => {
-        addToast(error.response.data.message, { appearance: "error" });
-      });
+        .then((response) => {
+          if (response.status === 200) {
+            setConsultant(response.data.data.docs);
+          } else {
+            addToast(response.data.message, {appearance: "error"});
+          }
+        })
+        .catch((error) => {
+          addToast(error.response.data.message, {appearance: "error"});
+        });
   }
 
   function debounce(txt) {
@@ -64,54 +60,69 @@ const PatientHomePage = (props) => {
       setSearchText(txt);
       props.history.push("/patient/topConsultants");
     }, 1000);
-  } 
+  }
 
+  function getNumberOfSpecialityToShow() {
+    if (window.screen.availWidth > 1200) {
+      return 14
+    } else if (window.screen.availWidth > 1023) {
+      return 8
+    } else {
+      return 6
+    }
+  }
   return (
-    <>
+      <>
         <Col lg="10" sm="10" xs="10" className='screen-768'>
           <Row className="search-container mobileviewSearch">
             <SearchInputWithIcon
-              className="patient-homepage-search"
-              placeholder="Search doctors"
-              onChange={(e) => debounce(e)}
+                className="patient-homepage-search"
+                placeholder="Search doctors"
+                onChange={(e) => debounce(e)}
             ></SearchInputWithIcon>
           </Row>
-          <Row style={{ marginTop: "32px" }}>
+          <Row style={{marginTop: "32px"}}>
             <Col>
-              <CarouselComponent sliders={slider} />
+              <CarouselComponent sliders={slider}/>
             </Col>
           </Row>
-          <Row style={{ marginTop: "42px", marginBottom: "32px" }}>
+          <Row style={{marginTop: "42px", marginBottom: "32px"}}>
             <Col>
               <span className="patient-homepage-text-h4">Specialities</span>
             </Col>
-            <Col style={{ textAlign: "right" }}>
-              <span className="patient-homepage-link-text " style={{cursor: 'pointer'}} onClick={(e) => props.history.push("/patient/specialities")}>View All</span>
+            <Col style={{textAlign: "right"}}>
+              {specialities.length > getNumberOfSpecialityToShow() && <span className="patient-homepage-link-text " style={{cursor: 'pointer'}}
+                    onClick={(e) => props.history.push("/patient/specialities")}>View All</span>}
             </Col>
           </Row>
           <Row className='specialities-container'>
-            {specialities && specialities.slice(0, window.screen.availWidth > 1200 ? 8 : 5 ).map((specialitie) => {
+            {specialities && specialities.slice(0, getNumberOfSpecialityToShow()).map((specialitie) => {
               return (
-                <SpecialityCard
-                  icon={specialitie.image}
-                  label={specialitie.title}
-                  setSearchText={debounce}
-                />
+                  <SpecialityCard
+                      icon={specialitie.image}
+                      label={specialitie.title}
+                      setSearchText={debounce}
+                  />
               );
             })}
+            { !specialities.length &&
+            <div className="empty-list-container">
+              <h4>No specialities found</h4>
+            </div>
+            }
           </Row>
-          <Row style={{ marginTop: "42px", marginBottom: "32px" }}>
+          <Row style={{marginTop: "42px", marginBottom: "32px"}}>
             <Col>
               <span className="patient-homepage-text-h4">Top Consultants</span>
             </Col>
             <Col style={{ textAlign: "right" }}>
-                <span className="patient-homepage-link-text " style={{cursor: 'pointer'}} onClick={(e) => props.history.push("/patient/topConsultants")}>View All</span>
+              {consultants.length >3 &&<span className="patient-homepage-link-text " style={{cursor: 'pointer'}} onClick={(e) => props.history.push("/patient/topConsultants")}>View All</span>}
             </Col>
           </Row>
-          <Row style={{ display: "flex", flexDirection: "row" }}>
+          <Row style={{display: "flex", flexDirection: "row"}}>
             {consultants && consultants.slice(0, 3).map((doctor) => {
               return (
-                <Grid container item lg={4} md={6} sm={6} xs={12} spacing={1}>
+                  <Grid container item lg={4} md={6} sm={6} xs={12} spacing={1}>
                     <SimilarDoctorsCard
                         id={doctor._id}
                         image={doctor.dp}
@@ -121,12 +132,17 @@ const PatientHomePage = (props) => {
                         qualifications={doctor.specialities}
                     />
 
-                </Grid>
+                  </Grid>
               );
             })}
+            { !consultants.length &&
+            <div className="empty-list-container">
+              <h4>No top Consultant found</h4>
+            </div>
+            }
           </Row>
         </Col>
-    </>
+      </>
   );
 };
 
