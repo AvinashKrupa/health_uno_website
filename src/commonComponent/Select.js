4f7cd78c6363036c3e45e5f700bd02f0e10336b9
Neel Patel <neel@cnetric.com>
2021-09-28 14:57:26 +0530

    Fixed HUNO-239

--- src/commonComponent/Select.js
+++ src/commonComponent/Select.js
@@ -5,7 +5,7 @@ const Selector = (props) => {
     <Form.Group>
       <br />
       <Form.Label>{props.label}</Form.Label>
-      <select className="form-select" id={props.id} onChange={(e) => props.handleSelect(e.target.value)}>
+      <select className="form-select" id={props.id} value={props.value} onChange={(e) => props.handleSelect(e.target.value)}>
         <option defaultValue={props.defaultValue}>{props.defaultValue}</option>
         {props.options.map((item) => (
           <option value={item}>{item}</option>
