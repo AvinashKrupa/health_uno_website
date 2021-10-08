e4d7a5636ee0a1780656e53bd222e86dc9d8616b
Neel Patel <neel@cnetric.com>
2021-10-08 12:25:29 +0530

    Fixed HUNO-351: Logout api in doctor website for doctor and patient

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,6 +2,8 @@ import SideNav, {NavIcon, NavItem, NavText} from '@trendmicro/react-sidenav';
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import {clearSession, getData} from "./storage/LocalStorage/LocalAsyncStorage";
 import { withRouter } from 'react-router-dom'
+import { useToasts } from 'react-toast-notifications';
+import {API, post} from './api/config/APIController';
 
 const sidebar = ['home', 'appointments', 'profile', 'reports']
 
@@ -15,6 +17,23 @@ const Sidebar = (props) => {
             defaultSelection = newRoutes[i];
         }
     }
+    const {addToast} = useToasts();
+
+    const handleLogout = () => {
+        post(API.LOGOUT)
+        .then(response => {
+          if (response.status === 200) {
+            
+            addToast(response.data.message, {appearance: 'success'});
+          } else {
+            addToast(response.data.message, {appearance: 'error'});
+          }
+        })
+        .catch(error => {
+          addToast(error.response.data.message, {appearance: "error"});
+        });
+    }
+
     return (
         <div className="sidebarMenu">
             <SideNav onSelect={(selected) => {
@@ -35,6 +54,7 @@ const Sidebar = (props) => {
 
                 if( selected === 'signOut') {
                     clearSession();
+                    handleLogout();
                     props.history.push(`${routeName}`);
                 }
 
