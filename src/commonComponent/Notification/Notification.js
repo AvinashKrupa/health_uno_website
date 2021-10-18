c83e6bea82e7a17031a2d6d7ec2c1c47f4a1ed36
Amit Varshney <amit.varshney1@yahoo.co.in>
2021-10-18 16:57:50 +0530

    HUNO-374: Added time for notification

--- src/commonComponent/Notification/Notification.js
+++ src/commonComponent/Notification/Notification.js
@@ -1,10 +1,12 @@
 import React from "react";
+import {timeSince} from "../../utils/utilities";
 
 const Notification = (props) => {
   return (
     <div className="notification-container">
         <div className="notification-title">{props?.item?.message}</div>
         <div className="notification-description">{props?.item?.message}</div>
+        <div className="notification-time">{timeSince(props?.item?.updated_at)}</div>
     </div>
   );
 };
